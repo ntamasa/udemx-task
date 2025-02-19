83d71878e1b35cfe10f70111c822ee369efe9f35
Tamás Nagy <ntamasalbert@gmail.com>
2025-02-19 17:48:40 +0100

    add navigation

---
+++ src/app/components/navigation/navigation.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-navigation',
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  templateUrl: './navigation.component.html',
+  styleUrl: './navigation.component.scss',
+})
+export class NavigationComponent implements OnInit {
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {}
+
+  changePage(): void {
+    const currentPath = this.getCurrentPath();
+    console.log(currentPath);
+    this.router.navigate([`/${!currentPath ? 'admin' : ''}`]);
+  }
+
+  getCurrentPath(): string {
+    return this.router.url.split('/')[1];
+  }
+}
